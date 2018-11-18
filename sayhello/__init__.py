3d68093c6e90d629d56458e24c6d990faf3d1d22
Tuna Hsu <tunahsu45@gmail.com>
2018-11-18 22:22:35 +0800

    Add Faker, Moment.js to create fake data and render local time

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
+from flask_moment import Moment
 
 app = Flask('sayhello')
 app.config.from_pyfile('setting.py')
@@ -10,6 +12,8 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from sayhello import views, errors, commands
--- sayhello/commands.py
+++ sayhello/commands.py
@@ -1,14 +1,27 @@
 import click
 from sayhello import app, db
 from sayhello.models import Message
+from faker import Faker
 
+#創建假資料
 @app.cli.command()
-@click.option('--drop', is_flag=True, help='Create after drop.')
-def initdb(drop):
-    """Initialize the database."""
-    if drop:
-        click.confirm('This operation will delete the database, do you want to continue?', abort=True)
-        db.drop_all()
-        click.echo('Drop tables.')
+@click.option('--count', default=20, help='Quantity of messages, default is 20.')
+def forge(count):
+    """Generate fake messages."""
+
+    db.drop_all()
     db.create_all()
-    click.echo('Initialized database.')
+
+    fake = Faker()
+    click.echo('Working...')
+
+    for i in range(count):
+        message = Message(
+            name=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year()
+        )
+        db.session.add(message)
+
+    db.session.commit()
+    click.echo('Created %d fake messages.' % count)
